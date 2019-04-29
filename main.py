bc03ce510035a1fb2039f90a58838650d43480f0
Jakub Raban <jakub.raban@gmail.com>
2019-04-29 18:06:07 +0200

    added updating cells

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.core.window import Window
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from game import Game
 
 
 class MenuScreen(Screen):
@@ -26,25 +27,49 @@ sm.transition = NoTransition()
 
 
 class SweepyGame(GridLayout):
-    def __init__(self, board_width, board_height, **kwargs):
+    def __init__(self, board_height, board_width, players, **kwargs):
         super(SweepyGame, self).__init__(**kwargs)
         self.cols = board_width
         self.rows = board_height
-        self.spacing = [2,2]
+        self.spacing = [2, 2]
+        global game
+        game = Game(board_height, board_width, players)
         self.all_tiles = dict()
         Window.size = 32 * self.cols - 2, 32 * self.rows - 2
         for i in range(self.rows):
             for j in range(self.cols):
-                self.all_tiles[(i, j)] = Image(source='images/tile_normal.png', width=30, height=30, size_hint_x=None, size_hint_y=None)
+                self.all_tiles[(i, j)] = Image(source='images/tile.png', width=30, height=30, size_hint_x=None, size_hint_y=None)
                 self.add_widget(self.all_tiles.get((i, j)))
-        self.all_tiles[(2, 4)].source = 'images/tile_flagged_ok.png'
-        self.all_tiles[(0, 2)].source = 'images/tile_two.png'
-        self.all_tiles[(5, 7)].source = 'images/tile_zero.png'
+        self.update_cell(0, 0)
+        self.update_cell(board_height - 1, 0)
+        self.update_cell(0, board_width - 1)
+        self.update_cell(board_height - 1, board_width - 1)
+
+    def update_cell(self, row, column):
+        filename = "images/tile"
+        our_cell = game.board.get_cell_by_indexes(row, column)
+        if our_cell.is_uncovered:
+            if our_cell.has_mine:
+                filename += "_mine"
+            else:
+                filename += ("_" + str(our_cell.mines_around))
+        if not our_cell.has_mine:
+            if our_cell.flagging_player is not None:
+                filename += "_flag"
+                if our_cell.has_mine:
+                    filename += "_ok"
+                else:
+                    filename += "_bad"
+            for player in game.players:
+                if player.get_position() == (row, column):
+                    filename += ("_player_" + player.color.value)
+        filename += ".png"
+        self.all_tiles[(row, column)].source = filename
 
 
 class SweepyApp(App):
     def build(self):
-        return SweepyGame(35, 25)
+        return SweepyGame(25, 25, 3)
 
 
 if __name__ == '__main__':
