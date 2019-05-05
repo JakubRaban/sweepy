d7e1ad31a61e23af265756ccbd2f430518804e9d
Bartek <bartek.strozik123@gmail.com>
2019-05-05 13:30:56 +0200

    added keyboard-event dispatcher

---
+++ player.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class Player:
+    def __init__(self, row, column, color):
+        self.row = row
+        self.column = column
+        self.score = 0
+        self.is_dead = False
+        self.color = color
+
+    def set_new_position(self, coords):
+        self.row, self.column = coords[0], coords[1]
+
+    def get_position(self):
+        return self.row, self.column
+
+    def add_points(self, delta):
+        self.score += delta
+
+
+class PlayerColor(Enum):
+    RED = "red"
+    BLUE = "blue"
+    GREEN = "green"
+    YELLOW = "yellow"
