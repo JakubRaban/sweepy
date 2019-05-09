09f884360f9fc7438e4da8ea7cff0422a53ee62f
Jakub Raban <jakub.raban@gmail.com>
2019-05-09 17:01:25 +0200

    scores are now updated

--- main.py
+++ main.py
@@ -35,13 +35,20 @@ class WholeWindow(BoxLayout):
         super().__init__(**kwargs)
         self.game = Game(board_height, board_width, players)
 
+        score_player_blue = Label(text='0', bold=True, color=[63 / 255, 115 / 255, 232 / 255, 0])
+        score_player_red = Label(text='0', bold=True, color=[203 / 255, 30 / 255, 30 / 255, 0])
+        score_player_green = Label(text='0', bold=True, color=[53 / 255, 219 / 255, 35 / 255, 0])
+        score_player_yellow = Label(text='0', bold=True, color=[255 / 255, 186 / 255, 0, 0])
+        self.score_labels = [score_player_blue, score_player_red, score_player_green, score_player_yellow]
+        permutation_matrix = [2, 1, 0, 3]
+        self.score_labels = self.score_labels[0:players-1]
+
         self.orientation = 'vertical'
         self.score_table = GridLayout(height=50, size_hint_y=None, rows=2, cols=2)
-        score_player_red = Label(text='0', bold=True, color=[203/256, 30/256, 30/256, 0])
-        self.score_table.add_widget(Label(text='0'))
+        self.score_table.add_widget(score_player_green)
         self.score_table.add_widget(score_player_red)
-        self.score_table.add_widget(Label(text='0'))
-        self.score_table.add_widget(Label(text='0'))
+        self.score_table.add_widget(score_player_blue)
+        self.score_table.add_widget(score_player_yellow)
         self.add_widget(self.score_table)
         self.game_grid = GameBoard(board_height, board_width, players)
         self.add_widget(self.game_grid)
@@ -54,7 +61,8 @@ class WholeWindow(BoxLayout):
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
 
     def update_labels(self):
-        pass
+        for index, label in enumerate(self.score_labels):
+            label.text = str(self.game.players[index].score)
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
@@ -87,6 +95,9 @@ class WholeWindow(BoxLayout):
         elif keycode[1] == 'g':
             self.game.flag_cell(1)
 
+        if keycode[1] in ['.', 'g']:
+            self.update_labels()
+
         for i in range(self.game_grid.rows):
             for j in range(self.game_grid.cols):
                 self.game_grid.update_cell(i, j, self.game)
@@ -108,11 +119,11 @@ class GameBoard(GridLayout):
             for j in range(self.cols):
                 self.all_tiles[(i, j)] = Image(source='images/tile.png', width=25, height=25, size_hint_x=None, size_hint_y=None)
                 self.add_widget(self.all_tiles.get((i, j)))
-        self.all_tiles[(0, 0)].source = 'images/tile_player_green.png'
+        self.all_tiles[(board_height - 1, 0)].source = 'images/tile_player_blue.png'
         if players >= 2:
-            self.all_tiles[(board_height - 1, 0)].source = 'images/tile_player_blue.png'
-        if players >= 3:
             self.all_tiles[(0, board_width - 1)].source = 'images/tile_player_red.png'
+        if players >= 3:
+            self.all_tiles[(0, 0)].source = 'images/tile_player_green.png'
         if players >= 4:
             self.all_tiles[(board_height - 1, board_width - 1)].source = 'images/tile_player_yellow.png'
 
@@ -140,7 +151,7 @@ class GameBoard(GridLayout):
 
 class SweepyApp(App):
     def build(self):
-        return WholeWindow(20, 25, 4)
+        return WholeWindow(20, 25, 2)
 
 
 if __name__ == '__main__':
